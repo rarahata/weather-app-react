f7908f83505a2c1f434ef427abd0c97761163140
rarahata <rei.arahata@gmail.com>
2022-01-09 17:54:16 +0000

    create basic html in 3 divs

--- src/App.js
+++ src/App.js
@@ -4,11 +4,48 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="search-engine">
       <form>
-        <input style={text} placeholder="Enter your city..."></input>
-        <input style={submit} value="Search"></input>
+        <input type="text" placeholder="Enter your city..."></input>
+        <input type="submit" value="Search"></input>
       </form>
+      </div>
+      <div className="weather-diaplay">
+      <h1>Tokyo</h1>
+      <p>Last updated: Monday 17:00</p>
+      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather-icon" />
+      <p>7 °C | °F</p>
+      <ul>
+        <li>
+          Broken Clouds
+        </li>
+        <li>
+          Humidity: 55%
+        </li>
+        <li>
+          Wind: 1 m/s
+        </li>
+      </ul>
+      </div>
+      <div className="weather-forecast">
+      <p>Tue</p>
+      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather-forecast-icon" />
+      <p>10 | 7</p>
+      <p>Tue</p>
+      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather-forecast-icon" />
+      <p>10 | 7</p>
+
+      <p>Tue</p>
+      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather-forecast-icon" />
+      <p>10 | 7</p>
+
+      <p>Tue</p>
+      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather-forecast-icon" />
+      <p>10 | 7</p>
+      </div>
+      <p>
+        <a href="https://github.com/rarahata/vanilla-weather-app">Open-Source Code</a> by Rei Arahata
+      </p>
     </div>
   );
 }
