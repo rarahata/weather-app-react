8ed6e5ec3f7e7ccbd6bd4a1d069188e0594fae93
rarahata <rei.arahata@gmail.com>
2022-01-10 13:12:43 +0000

    add props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App city="Tokyo"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
